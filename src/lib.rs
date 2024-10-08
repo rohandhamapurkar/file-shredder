9631d5f4daff2fe69c8edfeb710e22b8d2851b4f
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2024-10-08 21:08:16 +0530

    feat: added code for shredding directories

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use rand::Rng;
+use std::collections::VecDeque;
 use std::fs::{self, OpenOptions};
 use std::io::{self, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Instant;
 use std::{env, thread};
+use walkdir::WalkDir;
 
 #[macro_export]
 macro_rules! print_exit {
@@ -20,18 +22,21 @@ fn generate_random_array(length: u64) -> Vec<u8> {
     let length_usize = usize::try_from(length).unwrap_or(usize::MAX);
     let mut vec = vec![0; length_usize];
     rand::thread_rng().fill(&mut vec[..]);
-    return vec
+    return vec;
 }
 
-pub fn shred_file_parallel(path: PathBuf, passes: u32, threads: u32) -> io::Result<()> {
+pub fn shred_file(path: PathBuf, passes: u32, threads: u32) -> io::Result<()> {
     let start_time = Instant::now();
 
     let file_path = Arc::new(path);
+    let file_name = file_path.file_name().unwrap().to_str().unwrap();
     let file_size = file_path.metadata()?.len();
     let chunk_size = file_size / (threads as u64);
 
+    println!("Started shredding {}", file_name);
+
     for pass in 0..passes {
-        println!("Executing pass {}", pass + 1);
+        // println!("Executing pass {}", pass + 1);
         let mut handles = vec![];
 
         for i in 0..threads {
@@ -70,7 +75,33 @@ pub fn shred_file_parallel(path: PathBuf, passes: u32, threads: u32) -> io::Resu
     std::fs::remove_file(&*file_path)?;
 
     let total_duration = start_time.elapsed();
-    println!("File shredded and deleted in {:?}", total_duration);
+    println!("{} shredded and deleted in {:?}", file_name, total_duration);
+
+    Ok(())
+}
+
+pub fn shred_folder(dir_path: PathBuf, passes: u32, threads: u32) -> io::Result<()> {
+    let mut deque: VecDeque<PathBuf> = VecDeque::new();
+    for entry in WalkDir::new(&dir_path) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        // skip the root directory
+        if path.eq(&dir_path) {
+            deque.push_front(path.to_path_buf());
+            continue;
+        }
+
+        if path.is_file() {
+            shred_file(path.to_path_buf(), passes, threads)?
+        } else if path.is_dir() {
+            deque.push_front(path.to_path_buf());
+        }
+    }
+
+    // delete all the directories after shredding files.
+    std::fs::remove_dir_all(dir_path)?;
+    println!("Deleted directories");
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use file_shredder::get_src_path_from_args;
 use file_shredder::print_exit;
-use file_shredder::shred_file_parallel;
+use file_shredder::shred_file;
+use file_shredder::shred_folder;
 
 fn main() {
-    let file_path = get_src_path_from_args();
+    let src_path = get_src_path_from_args();
     let passes = 5; // Number of overwrite passes
 
-    match shred_file_parallel(file_path, passes, 5) {
-        Ok(_) => {}
-        Err(e) => print_exit!(e),
+    if src_path.is_file() {
+        if let Err(e) = shred_file(src_path, passes, 5) {
+            print_exit!(e);
+        }
+    } else if src_path.is_dir() {
+        if let Err(e) = shred_folder(src_path, passes, 5) {
+            print_exit!(e);
+        }
+    } else {
+        print_exit!(format!(
+            "Source path is neither a file nor a directory: {}",
+            src_path.display()
+        ));
     }
 }
