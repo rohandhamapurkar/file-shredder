d8935a4003e9dc66d412fd56bb0d0e7870e03d1e
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2024-10-13 11:52:11 +0530

    other: removed main fn error

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
 use std::error::Error;
 
+use file_shredder::errors::CustomError::PathNonExistErr;
 use file_shredder::get_args;
 use file_shredder::print_exit;
 use file_shredder::shred_file;
 use file_shredder::shred_folder;
-use file_shredder::errors::CustomError::PathNonExistErr;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let (src_path, passes) = get_args().unwrap_or_else(|e| {
         print_exit!(e);
     });
 
-
     let result: Result<(), Box<dyn Error>> = match () {
         _ if src_path.is_file() => shred_file(src_path, passes, 5),
         _ if src_path.is_dir() => shred_folder(src_path, passes, 5),
-        _ => Err(Box::new(PathNonExistErr))
+        _ => Err(Box::new(PathNonExistErr)),
     };
 
     if let Err(e) = result {
         print_exit!(e)
     }
-
-    Ok(())
 }
